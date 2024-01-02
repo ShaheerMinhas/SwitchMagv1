5b4d265d9934abb712902c26078c89020d4b4fe2
Shaheer <shaheer@pop-os.localdomain>
2024-01-03 01:23:20 +0500

    second

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Header from '../../components/home/header/Index'
-import BlogList from '../../components/home/bloglist'
-import {blogList} from '../../config/data'
+import React from 'react';
+import Header from '../../components/home/header/Index';
+import BlogList from '../../components/home/bloglist';
+import { blogList } from '../../config/data';
+import './styles.css'; // Import a CSS file for styling (create this file)
+
 const Home = () => {
   return (
-    <div>
+    <div className="home-container">
       <Header />
-      <BlogList blogs={blogList}/>
-
+      <div className='divider'></div>
+      <div className="blog-list-container">
+        <BlogList blogs={blogList} />
+      </div>
     </div>
-        )
-}
+  );
+};
 
-export default Home
+export default Home;
