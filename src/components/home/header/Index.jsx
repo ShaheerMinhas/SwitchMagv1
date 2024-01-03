ff6cccc4d224d0e3205598ef0d161c8320c17fce
Shaheer <shaheer@pop-os.localdomain>
2024-01-04 04:19:32 +0500

    initiaewl

--- src/components/home/header/Index.jsx
+++ src/components/home/header/Index.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
-import "./styles.css"
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './styles.css';
+
 const Header = () => {
   return (
     <header className='home-header'>
-        <h1>
-            <span>Switch Magazine</span></h1>
-            <p>Tech blog about cool FYPS</p>
+      <div className="header-content">
+        <div className="header-text">
+          <h1>
+            <span style={{ color: 'purple' }}>Switch Magazine</span>
+          </h1>
+          <p>Tech blog about cool FYPS</p>
+        </div>
+        <div className="join-button">
+          <Link to="/signin">
+            <button>Join Us</button>
+          </Link>
+        </div>
+      </div>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
