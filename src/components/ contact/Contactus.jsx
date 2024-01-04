995ea91011e685a54966460b2a96629b7a18bfda
ShaheerMinhas <132215946+ShaheerMinhas@users.noreply.github.com>
2024-01-04 20:46:39 +0500

    Update Contactus.jsx

--- src/components/ contact/Contactus.jsx
+++ src/components/ contact/Contactus.jsx
@@ -9,8 +9,8 @@ const ContactUs = () => {
         <form name="contact" netlify>
           <input type="hidden" name="form-name" value="contact" />
           <div className="form-group">
-            <label htmlFor="email">Email:
-            <input type="text" id="email" name="email" placeholder="Your email..." required /></label>
+            <label htmlFor="email">Email:</label>
+            <input type="text" id="email" name="email" placeholder="Your email..." required />
           </div>
           <div className="form-group">
             <label htmlFor="feedback">Feedback:</label>
