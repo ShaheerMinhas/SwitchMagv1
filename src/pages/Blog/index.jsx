9030e2dc7f8338be3f4a014ffbd215155f272072
Shaheer <shaheer@pop-os.localdomain>
2024-01-20 23:26:41 +0500

    second

--- src/pages/Blog/index.jsx
+++ src/pages/Blog/index.jsx
@@ -10,6 +10,8 @@ import { Typography, Grid } from '@mui/material';
 import Header from '../../components/home/header/Index';
 import ContactUs from '../../components/ contact/Contactus';
 import AudioPlayer from '../../components/Audio/Audioplayer';
+// ... (existing imports)
+
 const Blog = () => {
   const { id } = useParams();
   const [blog, setBlog] = useState(null);
@@ -25,50 +27,46 @@ const Blog = () => {
     <>
       <Header />
       <div className="upperPortion">
-      <div className='divider'></div>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={6}>
-          <div>
-            <Link className='blog-goBack' to='/'>
-              <span> &#8592;</span> <span>Go Back</span>
-            </Link>
-            {blog ? (
-              <div className='blog-wrap'>
-                <header>
-                  <p className='blog-date'>Published {blog.createdAt}</p>
-                  <Typography variant='h5'>
-                    <h1 className='blog-title'>{blog.title}</h1>
-                  </Typography>
-                </header>
-               <AudioPlayer/>
-              </div>
+        <div className='divider'></div>
+        <Grid container spacing={2}>
+          <Grid item xs={12} md={6}>
+            <div>
+              <Link className='blog-goBack' to='/'>
+                <span> &#8592;</span> <span>Go Back</span>
+              </Link>
+              {blog ? (
+                <div className='blog-wrap'>
+                  <header>
+                    <p className='blog-date'>Published {blog.createdAt}</p>
+                    <Typography variant='h5'>
+                      <h1 className='blog-title'>{blog.title}</h1>
+                    </Typography>
+                  </header>
+                  {/* AudioPlayer container with centered styling */}
+                  <div className='audio-player-container'>
+                    <AudioPlayer />
+                  </div>
+                </div>
+              ) : (
+                <EmptyList />
+              )}
+            </div>
+          </Grid>
 
-            ) : (
-              <EmptyList />
-            )}
-            
-          </div>
-
+          {/* Right part of the screen (Blog Picture) */}
+          <Grid item xs={12} md={6}>
+            {blog && <img src={blog.cover} alt='cover' style={{ width: '100%' }} />}
+          </Grid>
         </Grid>
-
-        {/* Right part of the screen (Blog Picture) */}
-        <Grid item xs={12} md={6}>
-          {blog && <img src={blog.cover} alt='cover' style={{ width: '100%' }} />}
-        </Grid>
-      </Grid>
-      
-
       </div>
       <div className="blog-text">
-      <Typography>
-  {blog && (
-    <ReactMarkdown>{blog.description}</ReactMarkdown>
-  )}
-  
-</Typography>
-
-                </div>
-                <ContactUs />
+        <Typography>
+          {blog && (
+            <ReactMarkdown>{blog.description}</ReactMarkdown>
+          )}
+        </Typography>
+      </div>
+      <ContactUs />
     </>
   );
 };
