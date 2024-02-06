42f6da48abc444959eb7e8cd80c4dc5305afb667
Syed Ali Hassan Zaidi <108427686+zaidiali9@users.noreply.github.com>
2024-02-06 10:38:26 +0500

    Authentication and the Articles are added in the firebase collection and the UI is fully dynamic

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getStorage } from 'firebase/storage';
-
+import { getAuth } from '@firebase/auth';
+import { getFirestore } from '@firebase/firestore';
 const firebaseConfig = {
     apiKey: "AIzaSyAEd85sgq3BxXRHo29YTSbkIjMKSDRHrjo",
     authDomain: "audioplayer-33956.firebaseapp.com",
@@ -13,5 +14,8 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const storage = getStorage(app);
-
+const auth = getAuth(app);
+const db=getFirestore(app);
 export { storage, app as default };
+export { auth };
+export { db }
